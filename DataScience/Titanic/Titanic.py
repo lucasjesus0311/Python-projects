5642fc98d4395fe8d14710757fd9ffb088568b98
LUCAS TEIXEIRA DE JESUS <lucasteixeira.ltj@gmail.com>
2023-05-25 00:14:09 -0300

    atualizado codigo

---
+++ DataScience/Titanic/Titanic.py
@@ -0,0 +1,5 @@
+# Importe a biblioteca pandas
+import pandas as pd
+
+# Carregue o conjunto de dados em um DataFrame
+dataset = pd.read_csv('https://github.com/lucasjesus0311/Python-projects/blob/main/DataScience/Titanic/Titanic.csv')
