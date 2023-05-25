e7c64df63d3d17fc3e991921f594df5080cf5eb7
LUCAS TEIXEIRA DE JESUS <lucasteixeira.ltj@gmail.com>
2023-05-25 00:29:31 -0300

    nova alteração feita peslo visual code

--- DataScience/Titanic/Titanic.py
+++ DataScience/Titanic/Titanic.py
@@ -4,4 +4,6 @@ import pandas as pd
 # Carregue o conjunto de dados em um DataFrame
 dataset = pd.read_csv('Projetos Python GIT\Python-projects\DataScience\Titanic\Titanic.csv')
 
-print(dataset.head() )     #Alteração direto  pelo git
+print(dataset.head() )     #Alteração direto  pelo git, 
+
+# linha adicionada pelo do VS Code
