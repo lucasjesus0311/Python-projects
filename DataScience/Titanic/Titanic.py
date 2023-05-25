5dc481524067f6a6803f131bcd8eaed3a5977f72
LUCAS TEIXEIRA DE JESUS <lucasteixeira.ltj@gmail.com>
2023-05-25 00:19:30 -0300

    Novo commit

--- DataScience/Titanic/Titanic.py
+++ DataScience/Titanic/Titanic.py
@@ -2,4 +2,6 @@
 import pandas as pd
 
 # Carregue o conjunto de dados em um DataFrame
-dataset = pd.read_csv('https://github.com/lucasjesus0311/Python-projects/blob/main/DataScience/Titanic/Titanic.csv')
+dataset = pd.read_csv('Projetos Python GIT\Python-projects\DataScience\Titanic\Titanic.csv')
+
+dataset.head()      
